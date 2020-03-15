f88b5bbff4f9849a55ae3690daa51092d66d1a0f
Svetlana Leovochkina <sleovochkina@gmail.com>
2020-03-15 19:00:05 +0300

    ready to be qualified

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
+var purchaseButtons = document.getElementsByClassName('purchase');
+var checkPopup = document.querySelector('.modal-check');
+var checkClose = checkPopup.querySelector('.modal-close');
+var toBuyingButton = checkPopup.querySelector('.back-to-buy-button');
+
+for (var i = 0; i < purchaseButtons.length; i++) {
+    purchaseButtons[i].addEventListener('click', function(evt) {
+      evt.preventDefault();
+      checkPopup.classList.add('modal-check-show');
+    });
+}
+
+checkClose.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  checkPopup.classList.remove('modal-check-show');
+});
+
+toBuyingButton.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  if (checkPopup.classList.contains('modal-check-show')) {
+    evt.preventDefault();
+    checkPopup.classList.remove('modal-check-show');
+  }
+});
+
+window.addEventListener('keydown', function(evt) {
+  if (evt.keyCode === 27) {
+    if (checkPopup.classList.contains('modal-check-show')) {
+      evt.preventDefault();
+      checkPopup.classList.remove('modal-check-show');
+    }
+  }
+});
+
 var emailPopupButton = document.querySelector('.email-button');
 var emailPopup = document.querySelector('.modal-email');
 var emailClose = emailPopup.querySelector('.modal-close');
@@ -76,7 +110,8 @@ window.addEventListener('keydown', function(evt) {
   }
 });
 
-let myTabs = function() {
+
+function myTabs() {
     let serviceTabs = document.querySelectorAll('.service-tab'),
         tabContent = document.querySelectorAll('.service-item'),
         tabName;
